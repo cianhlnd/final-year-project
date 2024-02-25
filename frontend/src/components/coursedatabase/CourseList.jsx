fa19b1d1aaba9cd165a5d2f239c1a608ee7e2b49
Cian <cian.hyland.2021@mumail.ie>
2024-02-25 22:26:22 +0000

    Course list with frontend functionality incorporated for containers

---
+++ frontend/src/components/coursedatabase/CourseList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../../styles/CourseList.css';
+
+function CourseList ({ courses, searchTerm, onSearch }) {
+  const filteredCourses = courses.filter((course) =>
+    course.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="course-list-container">
+      <div className="courses-wrapper">
+        {filteredCourses.map((course) => (
+          <div key={course.id} className="course-container">
+            <p>Title: {course.title}</p>
+            <p>Location: {course.location}</p>
+            <p>Date: {course.date}</p>
+            <a href={course.link} target="_blank" rel="noopener noreferrer">
+              Book Now
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CourseList;
