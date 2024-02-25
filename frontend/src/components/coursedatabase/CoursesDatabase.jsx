759c0e0b6044632be0702287e50446e8759796eb
Cian <cian.hyland.2021@mumail.ie>
2024-02-25 22:23:00 +0000

    Database for courses

---
+++ frontend/src/components/coursedatabase/CoursesDatabase.jsx
@@ -0,0 +1,68 @@
+const coursesData = [
+    {
+      id: 1,
+      title: 'Safeguarding 3',
+      location: 'Online',
+      date: '28/03/2024 - 28/03/2024',
+      link: 'https://faiconnect.ie/mycomet/private/seminars/14066133'
+    },
+    {
+      id: 2,
+      title: 'PDP 3',
+      location: 'Ballyglass FC, Michael Keaveney Park, Ballyglass, Co. Mayo',
+      date: '05/04/2024 - 06/04/2024',
+      link: 'https://faiconnect.ie/mycomet/private/seminars/14228434'
+    },
+    {
+      id: 3,
+      title: 'PDP 1',
+      location: 'Kildimo Community Centre, Coolbeg, Kildimo, County Limerick',
+      date: '06/04/2024 - 06/04/2024',
+      link: 'https://faiconnect.ie/mycomet/private/seminars/14338495'
+    },
+    {
+      id: 4,
+      title: 'PDP 2',
+      location: 'Donegal League HQ, Ballyare, Co. Donegal',
+      date: '07/04/2024 - 07/04/2024',
+      link: 'https://faiconnect.ie/mycomet/private/seminars/13600468'
+    },
+    {
+      id: 5,
+      title: '7v7',
+      location: 'Brackenstown Rd, Swords Glebe, Swords, Co. Dublin',
+      date: '09/04/2024 - 09/04/2024',
+      link: 'https://faiconnect.ie/mycomet/private/seminars/12878083'
+    },
+    {
+      id: 6,
+      title: 'PDP 3',
+      location: 'Ballaghaderreen FC, Toomanagh, Ballaghadereen, Co. Roscommon F45 KP52',
+      date: '12/04/2024 - 13/04/2024',
+      link: 'https://faiconnect.ie/mycomet/private/seminars/14456716'
+    },
+    {
+      id: 7,
+      title: 'PDP 2',
+      location: 'Aughrim Rangers AFC',
+      date: '13/04/2024 - 13/04/2024',
+      link: 'https://faiconnect.ie/mycomet/private/seminars/13941885'
+    },
+    {
+      id: 8,
+      title: 'Fundamentals of Goalkeeping',
+      location: 'Claremorris AFC, Concannon Park, Claremorris, Co. Mayo F12 X921',
+      date: '13/04/2024 - 13/04/2024',
+      link: 'https://faiconnect.ie/mycomet/private/seminars/14228886'
+    },
+    {
+      id: 9,
+      title: 'PDP 1',
+      location: 'Inishowen League HQ, Maginn Park, Buncrana, Co.Donegal',
+      date: '13/04/2024 - 13/04/2024',
+      link: 'https://faiconnect.ie/mycomet/private/seminars/14455963'
+    }
+  ];
+  
+  export default coursesData;
+  
