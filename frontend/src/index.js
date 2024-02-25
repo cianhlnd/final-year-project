963caecd5f758fc4c075609bf3487537397c5cfc
Cian <cian.hyland.2021@mumail.ie>
2024-02-25 22:30:57 +0000

    Updated index file with navbar functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,27 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./components/HomePage"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './styles/Navbar.css';
+import HomePage from './components/HomePage';
 import reportWebVitals from './reportWebVitals';
 import RegistrationForm from './components/RegistrationForm';
 import LoginForm from './components/LoginForm';
-import Profile from './components/Profile'
-import Courses from './components/coursedatabase/Courses'
+import Profile from './components/Profile';
+import Courses from './components/coursedatabase/Courses';
+import U8sSession from './components/sessionplans/u8sSession';
+import About from './components/About';
+
+function Navbar  () {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/courses">Courses</Link>
+        </li>
+        <li>
+          Sessions
+          <ul>
+            <li>
+              <Link to="/sessions/u8sSession">U8s Session</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<HomePage />} />
+          <Route path="registration" element={<RegistrationForm />} />
+          <Route path="login" element={<LoginForm />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="courses" element={<Courses />} />
+          <Route path="sessions/u8sSession" element={<U8sSession />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="home" element={<HomePage />} />
-      <Route path="registration" element={<RegistrationForm />} />
-      <Route path="login" element={<LoginForm />} />
-      <Route path="profile" element={<Profile />} />
-      <Route path="courses" element={<Courses />} />
-    </Routes>
-  </BrowserRouter>
-);
-
-reportWebVitals()
-
-
+root.render(<App />);
+reportWebVitals();
