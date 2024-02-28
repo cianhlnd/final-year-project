2b1246253986c96d3b5e5611935d75f2446a8447
Cian <cian.hyland.2021@mumail.ie>
2024-02-28 00:00:38 +0000

    Sample of about page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -4,8 +4,16 @@ function About () {
   return (
     <div>
       <h1>About Us</h1>
-      <p>Sample Text</p>
-      {/* Add more content as needed */}
+      <p>The goal of the website is simple. To improve make the
+        lives of volunteer coaches that much easier. I have combined both my passion for grassroots coaching
+        with my current course to create this application. I have 5 years experience coaching beginning my journey
+        working in academy football with my local club Lucan United completing TY work experience.
+        My journey then lead me to where I am today where I currently manage the u9s blue team for the same club
+        along with working for a global leader in football skill training called Coerver Coaching.
+        I am Garda Vetted, have my child safeguarding 1 completed, PDP 1, 2 and 3 courses completed
+        along with completing Coerver coaching workshops and a 7vs7 FAI workshop. With these qualifications
+        and my own experience I have developed custom session plans for coaches to use to improve the quality of their own sessions.
+      </p>
     </div>
   );
 };
