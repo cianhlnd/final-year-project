42dbcf2b8fc9ab0c15bde6d288fcd3f009ee5247
Cian <cian.hyland.2021@mumail.ie>
2024-03-05 22:53:29 +0000

    Updated about page

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
+import '../styles/About.css'
 
 function About () {
   return (
     <div>
-      <h1>About Us</h1>
-      <p>The goal of the website is simple. To improve make the
-        lives of volunteer coaches that much easier. I have combined both my passion for grassroots coaching
-        with my current course to create this application. I have 5 years experience coaching beginning my journey
-        working in academy football with my local club Lucan United completing TY work experience.
-        My journey then lead me to where I am today where I currently manage the u9s blue team for the same club
-        along with working for a global leader in football skill training called Coerver Coaching.
-        I am Garda Vetted, have my child safeguarding 1 completed, PDP 1, 2 and 3 courses completed
-        along with completing Coerver coaching workshops and a 7vs7 FAI workshop. With these qualifications
-        and my own experience I have developed custom session plans for coaches to use to improve the quality of their own sessions.
+      <h1 className = "about-header">About Us</h1>
+      <div  className = "about-text">
+      <p>Welcome to the Football Coaching Hub. My name is Cian Hyland. I am a current student in Maynooth University where I am
+      in the final year of my computer science degree. I was tasked with creating an interactive web application
+      for my final year project. After brainstorming a few ideas I ended up deciding on an area I am very passionate about,
+      grassroots football coaching. I have built up 6 years worth of football coaching experience, ranging from very young
+      kids all the way to u12s players. From these experiences I have gained gained a competency for football coaching
+      which I still use to this day. Currently I am a co-manager of the u9s Lucan United blue team in the DDSL as well as coach for 
+      2 Coerver performance academies. Throughout my studies as a coach I have researched many different coaching philosophies
+      and methodolgies and have added an array of coaching qualifcations onto this knowledge bank. 
       </p>
+      <ul>
+      My qualifcations include:
+      <li>PDP 1,2 and 3 courses</li>
+      <li>Garda Vetting</li>
+      <li>Child Safeguarding 1</li>
+      <li> Coerver Youth Diploma 1 as well as various workshops</li>
+      </ul>
+      <p>
+      I have developed this web application to tailor torwards age groups I feel comfortable sharing my knowledge base on 
+      through coaching them in the past. This is why this web application is for small-sided game coaches. The goal of this 
+      web application is simple, to make the lives of volunteer small-sided coaches easier.
+      </p>
+     
+      
+      </div>
+      
     </div>
   );
 };
