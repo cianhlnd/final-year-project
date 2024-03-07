a7f3973f8d46a761f84e747ce7228409ef3c3a3b
Cian <cian.hyland.2021@mumail.ie>
2024-03-07 01:37:22 +0000

    Updated to display review system and also allows users to download all session plans on a page through jszip

--- frontend/src/components/sessionplans/u8s-u9s/u8sDefendingSession.jsx
+++ frontend/src/components/sessionplans/u8s-u9s/u8sDefendingSession.jsx
@@ -1,26 +1,56 @@
 import React from 'react';
+import JSZip from 'jszip';
+import { saveAs } from 'file-saver';
 import ImageDownload from '../ImageDownload';
-import '../../../styles/Session.css'; 
+import ReviewComponent from '../ReviewComponent';
+import ReviewsDisplay from '../ReviewsDisplay';
+import '../../../styles/Session.css';
 
 function U8sDefendingSession() {
+  const sessionPlans = [
+    {
+      imageUrl: process.env.PUBLIC_URL + '/images/u8s-u9s/defending/0.png',
+      imageName: 'u8s-u9s-defending-warm-up.png',
+    },
+    {
+      imageUrl: process.env.PUBLIC_URL + '/images/u8s-u9s/defending/1.png',
+      imageName: 'u8s-u9s-defending-drill-1.png',
+    },
+    {
+      imageUrl: process.env.PUBLIC_URL + '/images/u8s-u9s/defending/2.png',
+      imageName: 'u8s-u9s-defending-drill-2.png',
+    },
+  ];
+
+  const downloadAllSessionPlans = async () => {
+    const zip = new JSZip();
+    for (const plan of sessionPlans) {
+      const response = await fetch(plan.imageUrl);
+      const blob = await response.blob();
+      zip.file(plan.imageName, blob, { binary: true });
+    }
+    zip.generateAsync({ type: 'blob' }).then((content) => {
+      saveAs(content, 'u8s-defending-session-plans.zip');
+    });
+  };
+
   return (
     <div>
-      <header className="u8s-header">Defending</header>
-      <ImageDownload
-        imageUrl={process.env.PUBLIC_URL + '/images/u8s-u9s/defending/0.png'}
-        imageName="u8s-u9s-defending-warm-up.png"
-        imageSize="500px"
-      />
-      <ImageDownload
-        imageUrl={process.env.PUBLIC_URL + '/images/u8s-u9s/defending/1.png'}
-        imageName="u8s-u9s-defending-drill-1.png"
-        imageSize="500px"
-      />
-      <ImageDownload
-        imageUrl={process.env.PUBLIC_URL + '/images/u8s-u9s/defending/2.png'}
-        imageName="u8s-u9s-defending-drill-2.png"
-        imageSize="500px"
-      />
+      <h1 className="u8s-header">Defending</h1>
+      {sessionPlans.map((plan, index) => (
+        <div key={index}>
+          <ImageDownload
+            imageUrl={plan.imageUrl}
+            imageName={plan.imageName}
+            imageSize="475px"
+          />
+          <div className = "review-container">
+          <ReviewComponent imageName={plan.imageName}/>
+          <ReviewsDisplay imageName={plan.imageName}/>
+          </div>
+        </div>
+      ))}
+      <button onClick={downloadAllSessionPlans} className = "all-button">Download All Plans</button>
     </div>
   );
 }
