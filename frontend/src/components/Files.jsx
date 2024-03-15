2ca8fb0789fa622a63d2fe0f9ddb52311c3ffa15
Cian <cian.hyland.2021@mumail.ie>
2024-03-15 13:27:13 +0000

    Updated file paths to see if it'll work with new web server

--- frontend/src/components/Files.jsx
+++ frontend/src/components/Files.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import '../styles/Files.css';
 
 const files = [
-  { name: 'FAI Tactics Board', url: '/files/FAI_Template_Virtual_Tactics_Board_Session planner_PDP.pptx' },
-  { name: 'FAI Player Development Plan', url: '/files/FAI_Player_Development_Plan.pdf' },
-  { name: 'FAI Pathway Plan', url: '/files/FAI_Pathway_Plan.pdf' },
+  { name: 'FAI Tactics Board', url: process.env.PUBLIC_URL + '/files/FAI_Template_Virtual_Tactics_Board_Session planner_PDP.pptx' },
+  { name: 'FAI Player Development Plan', url: process.env.PUBLIC_URL + '/files/FAI_Player_Development_Plan.pdf' },
+  { name: 'FAI Pathway Plan', url: process.env.PUBLIC_URL + '/files/FAI_Pathway_Plan.pdf' },
 ];
 
 function Files() {
   return (
     <div className="files-container">
-      <h1>Useful Files</h1>
+      <h1 className = "files-header">Useful Files</h1>
       <ul>
         {files.map((file, index) => (
           <li key={index}>
