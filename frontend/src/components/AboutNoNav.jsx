528a768e55d1987cbe53c236f71bd1a80df8449b
Cian <cian.hyland.2021@mumail.ie>
2024-03-15 00:32:06 +0000

    About page for homepage to link to added, contains no navbar so user's can't bypass account creation

--- frontend/src/components/AboutNoNav.jsx
+++ frontend/src/components/AboutNoNav.jsx
@@ -1,37 +1,40 @@
 import React from 'react';
 import '../styles/About.css'
+import { Link } from 'react-router-dom';
 
 function AboutNoNav () {
   return (
     <div>
       <h1 className = "about-header">About Us</h1>
+      <Link to="/homepage" style={{ color: 'white', textDecoration: 'none' }}>
+        <button className = "back-button">
+          Back
+        </button>
+      </Link>
       <div  className = "about-text">
-      <p>Welcome to the Football Coaching Hub. My name is Cian Hyland. I am a current student in Maynooth University where I am
-      in the final year of my computer science degree. I was tasked with creating an interactive web application
-      for my final year project. After brainstorming a few ideas I ended up deciding on an area I am very passionate about,
-      grassroots football coaching. I have built up 6 years worth of football coaching experience, ranging from very young
-      kids all the way to u12s players. From these experiences I have gained gained a competency for football coaching
-      which I still use to this day. Currently I am a co-manager of the u9s Lucan United blue team in the DDSL as well as coach for 
-      2 Coerver performance academies. Throughout my studies as a coach I have researched many different coaching philosophies
-      and methodolgies and have added an array of coaching qualifcations onto this knowledge bank. 
-      </p>
-      <ul>
-      My qualifcations include:
-      <li>PDP 1,2 and 3 courses</li>
-      <li>Garda Vetting</li>
-      <li>Child Safeguarding 1</li>
-      <li>Coerver Youth Diploma 1</li>
-      <li>Various workshops including FAI 7vs7, Coerver ball mastery and 1vs1</li>
-      </ul>
-      <p>
-      I have developed this web application to tailor torwards age groups I feel comfortable sharing my knowledge base on 
-      through coaching them in the past. This is why this web application is for small-sided game coaches. The goal of this 
-      web application is simple, to make the lives of volunteer small-sided coaches easier.
-      </p>
-     
-      
+        <p>Welcome to the Football Coaching Hub. My name is Cian Hyland. I am a current student in Maynooth University where I am
+          in the final year of my computer science degree. I was tasked with creating an interactive web application
+          for my final year project. After brainstorming a few ideas I ended up deciding on an area I am very passionate about,
+          grassroots football coaching. I have built up 6 years worth of football coaching experience, ranging from very young
+            kids all the way to u12s players. From these experiences I have gained gained a competency for football coaching
+          which I still use to this day. Currently I am a co-manager of the u9s Lucan United blue team in the DDSL as well as coach for 
+          2 Coerver performance academies. Throughout my studies as a coach I have researched many different coaching philosophies
+          and methodolgies and have added an array of coaching qualifcations onto this knowledge bank. 
+        </p>
+        <ul>
+          My qualifcations include:
+          <li>PDP 1,2 and 3 courses</li>
+          <li>Garda Vetting</li>
+          <li>Child Safeguarding 1</li>
+          <li>Coerver Youth Diploma 1</li>
+          <li>Various workshops including FAI 7vs7, Coerver ball mastery and 1vs1</li>
+        </ul>
+        <p>
+          I have developed this web application to tailor torwards age groups I feel comfortable sharing my knowledge base on 
+          through coaching them in the past. This is why this web application is for small-sided game coaches. The goal of this 
+          web application is simple, to make the lives of volunteer small-sided coaches easier.
+        </p>
       </div>
-      
     </div>
   );
 };
