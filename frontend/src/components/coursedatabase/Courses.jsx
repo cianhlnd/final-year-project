4ea88a2eea280568d76049266a9817d32e5bc587
Cian <cian.hyland.2021@mumail.ie>
2024-03-18 20:37:28 +0000

    Styled course search button

--- frontend/src/components/coursedatabase/Courses.jsx
+++ frontend/src/components/coursedatabase/Courses.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import CoursesDatabase from './CoursesDatabase';
 import CourseSearch from './CourseSearch';
 import CourseList from './CourseList';
+import '../../styles/CourseSearch.css'
 
 function Courses() {
   const [searchTerm, setSearchTerm] = useState('');
@@ -12,6 +13,9 @@ function Courses() {
 
   return (
     <div>
+       <button className="course-info-button">i
+        <span className="course-info-text">You will need to register with FAI Connect to book courses</span>
+      </button>
       <CourseSearch onSearch={handleSearch} />
       <CourseList courses={CoursesDatabase} searchTerm={searchTerm} />
     </div>
